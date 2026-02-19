35df673980f12404e732a176a02fc5c9d3ec38c0
André Gabriel Coetzee <andre.gabriel.coetzee@gmail.com>
2026-02-19 13:46:59 +0200

    Add some resources from freepik.com

--- MiningCalculator/MiningCalculator/ViewModels/AboutPageViewModel.cs
+++ MiningCalculator/MiningCalculator/ViewModels/AboutPageViewModel.cs
@@ -25,11 +25,6 @@ public partial class AboutPageViewModel : ObservableObject
         ProductName = "Mining Calculator";
         Version = $"Version {name.Version}";
         Description = "Vibe-Coded by André Coetzee and Claude Opus 4.6";
-        Copyright = GetAttribute<AssemblyCopyrightAttribute>(assembly)?.Copyright ?? string.Empty;
-    }
-
-    private static T? GetAttribute<T>(Assembly assembly) where T : Attribute
-    {
-        return assembly.GetCustomAttribute<T>();
+        Copyright = "Icons used from freepik.com";
     }
 }
